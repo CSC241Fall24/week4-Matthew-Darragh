3dded1be4605d6dfd4a5019dadfdce1de1812a6d
Matthew Darragh <MD956670@wcupa.edu>
2024-09-27 21:23:20 -0400

    Update Problem2.java

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -1,9 +1,26 @@
-
 public class Problem2 {
     public static ListNode insert(ListNode head, int val, int position) {
-        // TODO: Implement the insert method
-        // This method should insert a new node with the given value at the specified position
-        // If the position is out of bounds, insert the node at the end of the list
-        return null;
+        ListNode newNode = new ListNode(val);
+        
+        // If the list is empty or inserting at the beginning
+        if (head == null || position == 0) {
+            newNode.next = head;
+            return newNode;
+        }
+        
+        ListNode current = head;
+        int currentPosition = 0;
+        
+        // Traverse the list to find the insertion point
+        while (current.next != null && currentPosition < position - 1) {
+            current = current.next;
+            currentPosition++;
+        }
+        
+        // Insert the new node
+        newNode.next = current.next;
+        current.next = newNode;
+        
+        return head;
     }
 }
