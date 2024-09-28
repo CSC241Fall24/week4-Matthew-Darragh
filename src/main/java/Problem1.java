97835ff66cd36a56a9e8c49004aed1aab3e95044
Matthew Darragh <MD956670@wcupa.edu>
2024-09-27 22:36:20 -0400

    2

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -5,17 +5,16 @@ public class Problem1 {
             return l2;
         }
         
-        ListNode current = l1;
         
         // Traverse to the end of l1
-        while (current.next != null) {
-            current = current.next;
+        while (l1.next != null) {
+            l1 = l1.next;
         }
         
         // Append copies of l2 nodes to l1
         while (l2 != null) {
-            current.next = new ListNode(l2.val);
-            current = current.next;
+            l1.next = new ListNode(l2.val);
+            l1 = l1.next;
             l2 = l2.next;
         }
         
