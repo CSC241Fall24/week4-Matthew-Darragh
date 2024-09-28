cfe6f422a13372f9f8e55713e506b8f881aa6040
Matthew Darragh <MD956670@wcupa.edu>
2024-09-27 21:23:16 -0400

    Update Problem1.java

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -4,6 +4,19 @@ public class Problem1 {
         // TODO: Implement the concatenate method
         // This method should concatenate l2 to l1 and return l1
         // Remember to create new nodes instead of directly linking l1 to l2
-        return null;
+        if(l1 == null){
+            return l2;
+        }
+        else if(l2 == null){
+            return l1;
+        }
+        listnode temp = null;
+        temp = l1;
+        while(l2 != null){
+            temp.next = l2;
+            l2 = l2.next;
+            temp = temp.next;
+        }
+        return temp;
     }
 }
